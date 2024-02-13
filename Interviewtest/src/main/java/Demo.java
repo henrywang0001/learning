b17f38da7d35324858c2eb2fed9f28860d129c6e
henry <henrywang0001@yahoo.ca>
2024-02-13 15:01:57 -0500

    test

---
+++ Interviewtest/src/main/java/Demo.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Demo {
+
+    public static void main(String args[]) {
+
+        System.out.println("Hello, World !")
+
+
+    }
+
+
+}
+
